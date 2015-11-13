793f67eac9908c38db1b60d8aba3bded4fcbaa11
kanbara <akihiro.kanbara@l-is-b.com>
2015-11-13 12:47:41 +0900

    first commit

---
+++ app/src/main/java/com/example/kanbara/sensorget/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.kanbara.sensorget;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TextView txt01 = (TextView)findViewById(R.id.txt01);
+
+        StringBuilder strBuild = new StringBuilder();
+        SensorManager mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        List<Sensor> list = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        for (Sensor sensor : list) {
+            strBuild.append(sensor.getType());
+            strBuild.append(",");
+            strBuild.append(sensor.getName());
+            strBuild.append(",");
+            strBuild.append(sensor.getVendor());
+            strBuild.append("\n");
+        }
+        txt01.setText(strBuild.toString());
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
